a415fed3bbd7c1a057b38d4ca58181f314d292b8
mbarrett <mbarrett@opentable.com>
2015-03-10 16:12:29 +0000

    Adding call for Reservation Status

---
+++ ApiTool.core/Builders/Criteria/ReservationStatusCriteriaBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Net.Http;
+using apitool.core.Models.Criteria;
+using apitool.core.Models.Parameters;
+
+namespace apitool.core.Builders.Criteria
+{
+    public class ReservationStatusCriteriaBuilder : ICallCriteriaBuilder
+    {
+        public ICallCriteria Build(CallParameters parameters)
+        {
+            var url = string.Format("{0}.ashx/reservation/?st=0&pid={1}&rid={2}&conf={3}",
+                           parameters.ApiVersion,
+                           ConfigurationManager.AppSettings["PartnerId"],
+                           parameters.RestaurantId,
+                           parameters.ConfirmationNumber);
+
+            return new CallCriteria
+                {
+                    CallParameters = new CallParameters
+                        {
+                            Url = url,
+                            HttpMethod = HttpMethod.Get
+                        }
+                };
+        }
+    }
+}
